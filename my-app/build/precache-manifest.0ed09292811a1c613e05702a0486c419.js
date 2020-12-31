bbbf79193cf60cecd7e1f9f455b4d4a791f0a78a
Pharez Varlack <varlack17@hotmail.com>
2020-12-30 22:32:47 -0500

    Included build file

---
+++ my-app/build/precache-manifest.0ed09292811a1c613e05702a0486c419.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b9b5eb78d3aea77173674293eb35a83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dcc065e5c32748c8836",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5ea17793f515d164cc16",
+    "url": "/static/css/main.07f361d4.chunk.css"
+  },
+  {
+    "revision": "6dcc065e5c32748c8836",
+    "url": "/static/js/2.e5239599.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.e5239599.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ea17793f515d164cc16",
+    "url": "/static/js/main.f81b22c2.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
