5ff2aba4179d20d5e31f074b932de259bd74a46c
Pharez Varlack <varlack17@hotmail.com>
2021-01-14 06:16:34 -0500

    Added total duty dates table for RD output

--- backend/word_table_calendar.py
+++ backend/word_table_calendar.py
@@ -35,48 +35,84 @@ class WordTable():
 	def write_to_table(self):
 		document = Document()
 		
-		rows = len(self.duty_dates) + 1
+		drows = len(self.duty_dates) + 1
+		trows = len(self.total_days) + 1
+		
 		styles = document.styles
-		table = document.add_table(rows = rows, cols = 5)
-		table.style = styles['Table Grid']
-		headers = ['Week', 'Dates', 'RD', 'Management', 'Central Office']
+		
+		duty_table = document.add_table(rows=drows, cols=5)
+		document.add_page_break()
+		total_table = document.add_table(rows=trows, cols=3)
+
+		duty_table.style = styles['Table Grid']
+		total_table.style = styles['Table Grid']
+
+		dheaders = ['Week', 'Dates', 'RD', 'Management', 'Central Office']
+		theaders = ['Name', 'Weekdays', 'Weekends']
 
 		#-----Table Formatting-----
-		for i in range(1, rows, 2):
-			row = table.rows[i].cells[3]
+		for i in range(1, drows, 2):
+			row = duty_table.rows[i].cells[3]
 			try:
-				row2 = table.rows[i + 1].cells[3]
+				row2 = duty_table.rows[i + 1].cells[3]
 				row.merge(row2)
 			except IndexError:
 				pass
 
-		table.cell(0, 1).width = Inches(2.5)
-		table.cell(0, 1).vertical_alignment = WD_ALIGN_VERTICAL.CENTER
+		duty_table.cell(0, 1).width = Inches(2.5)
+		duty_table.cell(0, 1).vertical_alignment = WD_ALIGN_VERTICAL.CENTER
 		
+
+		total_table.cell(0, 1).width = Inches(2.5)
+		total_table.cell(0, 1).vertical_alignment = WD_ALIGN_VERTICAL.CENTER
+
 		for cell in range(5):
 			header_fill = parse_xml(r'<w:shd {} w:fill="{}"/>'.format(nsdecls('w'), HEADER_FILL))
-			table.rows[0].cells[cell]._tc.get_or_add_tcPr().append(header_fill)
+			try:
+				duty_table.rows[0].cells[cell]._tc.get_or_add_tcPr().append(header_fill)
+				total_table.rows[0].cells[cell]._tc.get_or_add_tcPr().append(header_fill)
+			
+			except IndexError:
+				pass
 
-		for i in range(1, rows, 2):
-			row = table.rows[i]
+		for i in range(1, drows, 2):
+			row = duty_table.rows[i]
 			
 			for j in range(5):
 				cell_fill = parse_xml(r'<w:shd {} w:fill="{}"/>'.format(nsdecls('w'), COLOR1))
 				row.cells[j]._tc.get_or_add_tcPr().append(cell_fill)
 
+		for i in range(1, trows, 2):
+			row = total_table.rows[i]
+
+			for j in range(3):
+				cell_fill = parse_xml(r'<w:shd {} w:fill="{}"/>'.format(nsdecls('w'), COLOR1))
+				row.cells[j]._tc.get_or_add_tcPr().append(cell_fill)
+
 		#-----Fill Header Info-----
-		for i in range(5):
-			text = headers[i]
-			p = table.rows[0].cells[i].add_paragraph(text + "\n")
+		for i in range(len(dheaders)):
+			text = dheaders[i]
+			p = duty_table.rows[0].cells[i].add_paragraph(text + "\n")
 			p.alignment = WD_ALIGN_PARAGRAPH.CENTER
 		
+		for i in range(len(theaders)):
+			text = theaders[i]
+			p = total_table.rows[0].cells[i].add_paragraph(text + "\n")
+			p.alignment = WD_ALIGN_PARAGRAPH.CENTER	
+
 		#------Write to Table-------
 		for i in range(len(self.duty_dates.items())):
 			date_info = list(self.duty_dates.items())[i]
 			row_info = ["Week %s" % (i + 1), date_info[0], date_info[1]]
 
 			for j in range(len(row_info)):
-				table.rows[i + 1].cells[j].text = row_info[j]
-				header_fill = parse_xml(r'<w:shd {} w:fill="5b95f9"/>'.format(nsdecls('w')))
+				duty_table.rows[i + 1].cells[j].text = row_info[j]
+
+		for i in range(len(self.total_days)):
+			x = list(self.total_days.items())
+			row_info = [x[i][0], x[i][1]['weekdays'], x[i][1]['weekends']]
+			
+			for j in range(len(row_info)):
+				total_table.rows[i + 1].cells[j].text = str(row_info[j])
 
 		document.save(self.path)
